5f80727ed6c05aef3117857478f79c5bff361fab
Leif Greiss <helter1234@gmail.com>
2017-06-25 13:50:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ function create(data) {
             div.transition()		
                 .duration(200)		
                 .style("opacity", .9);		
-            div.html(d.State + "<br/>" + "Population: " + d.Population + "<br/>" + "Fatal Crashes: " + d.Fatalcrashes + "<br/>" + "Deaths: " + d.Deaths)	
+            div.html(d.State + "<br/>" + "Population: " + d.Population + "<br/>" + "Fatal Crashes: " + d.Fatalcrashes + "<br/>" + "Deaths: " + d.Deaths + "<br/>" + "Deaths Per 100,000 Population: " + d.dbp)	
                 .style("left", (d3.event.pageX) + "px")		
                 .style("top", (d3.event.pageY - 28) + "px");})
 		//Creates mouseout function that makes the tooltip disappear
